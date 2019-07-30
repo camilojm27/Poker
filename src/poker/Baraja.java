6841d4c95e116a7a001bd12297a1aca31eaf2953
Camilo José Mezú Mina <camilo272001@gmail.com>
2019-07-30 15:56:01 -0500

    jh

---
+++ src/poker/Baraja.java
@@ -0,0 +1,11 @@
+package poker;
+
+import java.util.ArrayList;
+
+
+
+public class Baraja {
+    Baraja(){
+
+    }
+}
---
+++ src/poker/Carta.java
@@ -0,0 +1,17 @@
+package poker;
+
+public class Carta {
+    private String valor;
+    private String tipo;
+    private String source;
+
+
+    Carta(String valor, String tipo){
+
+    }
+
+
+    public String getSource() {
+        return source;
+    }
+}
